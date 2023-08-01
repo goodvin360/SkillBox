22980ac5120013eff3df14ea4f91ea30e8c9eef9
goodvin360 <goodvin360@yandex.rugit config --global user.email goodvin360@yandex.rugit config --global user.email goodvin360@yandex.ru>
2023-08-02 00:59:01 +0300

    02.08.2023 home 1 ditch

--- M33/M33_Task_3/main.cpp
+++ M33/M33_Task_3/main.cpp
@@ -2,77 +2,84 @@
 #include "vector"
 #include "random"
 #include "sstream"
+#include "typeinfo"
 
 template<typename T1, typename T2>
-struct Register
+class MyClass
 {
-    T1 key;
-    T2 value;
 
 public:
+    MyClass(){};
 
-    Register<T1,T2> aRegister(const T1& inKey, const T2& inValue)
+    MyClass(T1 inKey, T2 inValue)
     {
-        Register<T1,T2> reg;
-        reg.key=inKey;
-        reg.value=inValue;
-        return reg;
-    };
+        key = inKey;
+        value = inValue;
+    }
+    T1 key;
+    T2 value;
+    std::vector<MyClass> myVec;
 
-    std::vector<Register<T1,T2>> vec;
+};
+
+template<typename T>
+class MyClass2
+{
+private:
+
+public:
+    std::vector<T> vec;
+    MyClass2() = default;
+//    MyClass var;
 };
 
 
 int main() {
     std::cout << "Task 3" << std::endl;
 
-    srand(time(nullptr));
-
-    int a;
-
-    /*Register<int,std::string> reg;
-    reg.vec.push_back(reg.aRegister(10,"10"));
-
-    for (int i=0; i<reg.vec.size(); i++)
-    {
-        std::cout << reg.vec[i].key << " " << reg.vec[i].value << std::endl;
-    }*/
-
+    MyClass<int,std::string> a;
 
     for (int i=0; i<10; i++)
     {
-        int a=rand()%100;
-        double b=rand()%50;
-        std::stringstream tmp;
-        std::string c;
-        tmp << a << b;
-        c = tmp.str();
-//        std::cout << a << " " << b << " " << c << std::endl;
-
-        if (rand()%10+1 > 5)
-        {
-            Register<double,std::string> reg;
-            reg.vec.push_back(reg.aRegister(a,c));
-
-            for (int i=0; i<reg.vec.size(); i++)
-            {
-                std::cout << reg.vec[i].key << " " << reg.vec[i].value << std::endl;
-            }
-        }
-        else
-        {
-            Register<std::string, double> reg;
-            reg.vec.push_back(reg.aRegister(c,b));
-
-            for (int i=0; i<reg.vec.size(); i++)
-            {
-                std::cout << reg.vec[i].key << " " << reg.vec[i].value << std::endl;
-            }
-        }
+        MyClass<int,std::string> tmp;
+        tmp.key = i;
+        tmp.value = "lalala";
+        a.myVec.push_back(tmp);
     }
 
+    for (int i=0; i<a.myVec.size(); i++)
+    {
+        std::cout << a.myVec[i].key << " " << a.myVec[i].value << std::endl;
+    }
 
+    double rrr = 0;
+    MyClass<int,std::string> b;
 
+    MyClass2<MyClass<int,std::string>> p;
+    p.vec.push_back(a);
 
     return 0;
 }
+
+/*#include <iostream>
+#include "vector"
+#include "random"
+#include "sstream"
+#include "map"
+#include "variant"
+
+
+template <typename T1, typename T2>
+struct Register
+{
+
+};
+
+int main() {
+    std::cout << "Task 3" << std::endl;
+
+    std::variant<int,double,std::string> var;
+
+    return 0;
+}*/
+
