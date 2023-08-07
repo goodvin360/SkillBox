d31f243cfcf3c8b8e0aeafdf001ce4028b44ea29
goodvin360 <goodvin360@yandex.rugit config --global user.email goodvin360@yandex.rugit config --global user.email goodvin360@yandex.ru>
2023-08-08 01:37:59 +0300

    08.08.2023 mod.24 task 3

--- M24/M24_Task_3/main.cpp
+++ M24/M24_Task_3/main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include "ctime"
 #include "iomanip"
+#include <cstdlib>
+#include <unistd.h>
+#include <thread>
 
 
 int main() {
     std::cout << "Task 3" << std::endl;
 
-    time_t t_delta,t_start,t_end,t_now;
+    time_t t_delta;
     t_delta = time(nullptr);
     tm delta = *localtime(&t_delta);
     std::cout << "Enter time"<<std::endl;
@@ -18,6 +21,7 @@ int main() {
         delta.tm_min=(int)(t*0.0167)%60;
         delta.tm_sec=t%60;
         std::cout <<delta.tm_min<<":"<<delta.tm_sec<<std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
         t--;
     }
     std::cout << "DING! DING! DING!"<<std::endl;
